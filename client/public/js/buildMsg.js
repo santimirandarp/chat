04c2e38a405f57a55a39fc1b0764f7285f449e51
Santi Miranda <santimir@protonmail.com>
2022-05-15 21:09:01 +0200

    reorganizing

---
+++ client/public/js/buildMsg.js
@@ -0,0 +1,43 @@
+import { msgDate, htmlCreate, createSelect } from './utils.js';
+export class Message {
+    constructor(data) {
+        const { _id, tid, createdAt, msg } = data;
+        if (_id)
+            this._id = _id;
+        if (tid)
+            this.tid = tid;
+        if (createdAt instanceof Date) {
+            this.createdAt = createdAt;
+        }
+        else {
+            this.createdAt = new Date();
+        }
+        this.msg = msg;
+    }
+}
+/** Create a list item from message.
+ * This function is triggered by different actions.
+ * 1. when sender hits submit:`msg,createdAt,tid` are defined.
+ * 2. when received from DB. `msg, createdAt, _id` are defined.
+ * Once this is linked with a unique username/socket,
+ * they will be able to act on the message.
+ */
+export const msgToHTML = (data) => {
+    const { msg: m, createdAt, _id, tid, own } = data;
+    // we build up the message HTML
+    const msg = htmlCreate('li', {
+        'data-id': _id || '',
+        'data-tid': tid || '',
+    });
+    const date = htmlCreate('span', { class: 'time' });
+    date.innerText = msgDate(createdAt);
+    const msgBody = document.createElement('span');
+    msgBody.innerText = m;
+    //prepend elements to list item
+    msg.prepend(date, msgBody);
+    if (!own)
+        return msg;
+    msg.append(createSelect());
+    return msg;
+};
+export const HTMLToDOM = (html, target, type = 'append') => (type === 'prepend' ? target.prepend(html) : target.append(html));
---
+++ client/public/js/entry.js
@@ -0,0 +1,71 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+import { io } from 'socket.io-client';
+import { messages, form, ul, prevMsgs, input } from './elements.js';
+import { Message, msgToHTML, HTMLToDOM } from './buildMsg.js';
+import { getLastMsgsAsync, getMsgsAsync } from './fetch.js';
+import { replaceTId, messageSelectInteraction, tempId } from './utils.js';
+const uri = 'http://localhost:3000';
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        //open socket to uri
+        const socket = io();
+        yield getLastMsgsAsync(uri + '/messages');
+        prevMsgs.click = () => {
+            const l = ul.children.length;
+            getMsgsAsync(`${uri}/messages`, l, l + 10);
+        };
+        /*
+           on input submit append msg to DOM and
+           send to server. On server OK add _id
+           and enable + CRUD operations
+         */
+        form.addEventListener('submit', function (e) {
+            e.preventDefault();
+            //1. collect the value and reset field
+            const msg = input.value;
+            if (!msg)
+                return;
+            input.value = '';
+            //2. create the HTML list item
+            const toSave = new Message({ msg, tid: tempId(messages) });
+            const li = msgToHTML(Object.assign(Object.assign({}, toSave), { own: true }));
+            //3.A for the sender, append right away, as a preview
+            ul.append(li);
+            //readjust height
+            ul.scrollTo(0, ul.scrollHeight);
+            //4.emit the message to server
+            socket.emit('save message', { msg: toSave.msg, tid: toSave.tid });
+            // Append an event listener to that message
+            const msgOptions = li.querySelector('.msgOpts');
+            msgOptions.addEventListener('change', messageSelectInteraction(socket));
+        });
+        //5.change temporary id to permanent
+        socket.on('message saved', ({ _id, tid }) => {
+            const l = messages.length;
+            for (let i = 1; i < 100; i++) {
+                console.log(messages[l - i].getAttribute('data-tid'), typeof (tid));
+                if (i > l) {
+                    return;
+                }
+                else if (messages[l - i].getAttribute('data-tid') === tid) {
+                    return replaceTId(messages[l - i], _id);
+                }
+            }
+            return;
+        });
+        // client listens for new messages
+        socket.on('new message', (data) => {
+            HTMLToDOM(msgToHTML(data), ul);
+            ul.scrollTo(0, ul.scrollHeight);
+        });
+    });
+}
+main().catch((e) => console.log(e));
