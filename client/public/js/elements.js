c98ebf0c69b3bc5c374acbf7ad5caa11cc7bb163
Santi Miranda <santimir@protonmail.com>
2022-05-16 13:00:48 +0200

    html linter and prettier

--- client/public/js/elements.js
+++
@@ -1,5 +0,0 @@
-export const form = document.querySelector("form");
-export const ul = document.getElementById("messages");
-export const messages = ul.children;
-export const input = document.getElementById("m");
-export const prevMsgs = document.querySelector("button#wantMore");
